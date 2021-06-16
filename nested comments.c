c8c0b4f80c2f73d8e6240eb7968cdf3dbd81a606
Bha-vani <85944086+Bha-vani@users.noreply.github.com>
2021-06-16 21:08:41 +0530

    Create nested comments.c

---
+++ nested comments.c
@@ -0,0 +1,10 @@
+//write a programme for nested comments.what are the error & how to resolve it//
+#include<stdio.h>
+int main()
+{
+  /*programme for nested comments
+/*printf("hello world\n");*/  
+printf("hii hellol\n");*/
+}
+
+Output:expected expression before '\' token resolve by using remove the inside comment and put the '//' in between programme.
